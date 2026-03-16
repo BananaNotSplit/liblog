627f0c04bf07ac5ac4527511fd646a3db8749e8f
BananaNotSplit <98235195+BananaNotSplit@users.noreply.github.com>
2026-03-16 19:16:46 -0400

    Replaced `StaticString` with `String`.

--- Sources/liblog/liblog.swift
+++ Sources/liblog/liblog.swift
@@ -22,75 +22,75 @@ public struct Log: Sendable {
 #endif
 
 #if canImport(OSLog)
-	public func debug(_ message: StaticString) {
+	public func debug(_ message: String) {
 		native.debug("\(message)")
 	}
 
-	public func info(_ message: StaticString) {
+	public func info(_ message: String) {
 		native.info("\(message)")
 	}
 
-	public func log(_ message: StaticString) {
+	public func log(_ message: String) {
 		native.log("\(message)")
 	}
 
-	public func warn(_ message: StaticString) {
+	public func warn(_ message: String) {
 		native.warning("\(message)")
 	}
 
-	public func error(_ message: StaticString) {
+	public func error(_ message: String) {
 		native.error("\(message)")
 	}
 
-	public func critical(_ message: StaticString) {
+	public func critical(_ message: String) {
 		native.critical("\(message)")
 	}
 
-	public func fault(_ message: StaticString) {
+	public func fault(_ message: String) {
 		native.fault("\(message)")
 	}
 
-	public func trace(_ message: StaticString) {
+	public func trace(_ message: String) {
 		native.trace("\(message)")
 	}
 
-	public func notice(_ message: StaticString) {
+	public func notice(_ message: String) {
 		native.notice("\(message)")
 	}
 #else
-	public func debug(_ message: StaticString) {
+	public func debug(_ message: String) {
 		print("[DBG ]: \(message)")
 	}
 
-	public func info(_ message: StaticString) {
+	public func info(_ message: String) {
 		print("[INFO]: \(message)")
 	}
 
-	public func log(_ message: StaticString) {
+	public func log(_ message: String) {
 		print("[LOG ]: \(message)")
 	}
 
-	public func warn(_ message: StaticString) {
+	public func warn(_ message: String) {
 		print("[WARN]: \(message)")
 	}
 
-	public func error(_ message: StaticString) {
+	public func error(_ message: String) {
 		print("[EROR]: \(message)")
 	}
 
-	public func critical(_ message: StaticString) {
+	public func critical(_ message: String) {
 		print("[CRIT]: \(message)")
 	}
 
-	public func fault(_ message: StaticString) {
+	public func fault(_ message: String) {
 		print("[FALT]: \(message)")
 	}
 
-	public func trace(_ message: StaticString) {
+	public func trace(_ message: String) {
 		print("[TRCE]: \(message)")
 	}
 
-	public func notice(_ message: StaticString) {
+	public func notice(_ message: String) {
 		print("[NOTE]: \(message)")
 	}
 #endif
